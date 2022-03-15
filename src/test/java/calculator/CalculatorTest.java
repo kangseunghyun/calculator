7040575b51224dcbf26111e065b922b93e1f7bd3
kang <tltlrl@naver.com>
2022-03-15 15:23:38 +0900

    1

---
+++ src/test/java/calculator/CalculatorTest.java
@@ -0,0 +1,33 @@
+package calculator;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class CalculatorTest {
+
+	
+	@Test
+	public void testAdd() {
+		assertEquals("2", Calculator.add(1, 2));
+	}
+	
+	
+	@Test
+	public void testSubtract() {
+		
+	}
+	
+	@Test
+	public void testMultiply() {
+		
+	}
+	
+	@Test
+	public void testDivide() {
+		
+	}
+
+
+}
