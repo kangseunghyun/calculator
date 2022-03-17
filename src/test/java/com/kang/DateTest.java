c556446a7a70816856eebb07dc9bc51eff8773db
kangseunghyun <tltlrl@naver.com>
2022-03-17 14:05:55 +0900

    Create DateTest.java

---
+++ src/test/java/com/kang/DateTest.java
@@ -0,0 +1,24 @@
+package com.kang;
+
+import static org.junit.Assert.*;
+
+import java.util.Date;
+
+import org.junit.Test;
+
+public class DateTest {
+
+	@Test
+	public void testDate() {
+		Date date = new Date();
+		Class<? extends Date> class1 = date.getClass();
+		String name = class1.getName();
+		assertEquals("java.util.Date", name);
+	}
+	
+	@Test
+	public void testTimestamp() {
+		assertTrue(new Date().getTime() > 1647493019183L);
+	}
+
+}
